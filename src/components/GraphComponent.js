f3169de2bac2972266ba0138c8e609070253f2e3
sweetaloe <sweetaloe@yandex.ru>
2020-04-03 21:07:20 +0500

    Add data files

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -5,16 +5,20 @@ import Highcharts from 'highcharts';
 require('highcharts/modules/exporting')(Highcharts);
 require('highcharts/modules/export-data')(Highcharts);
 require('highcharts/modules/offline-exporting')(Highcharts);
-
+//onClick={this.updateSeries.bind(this)}>Построить график</but
 export default class GraphComponent extends Component {
+
 
     constructor(props) {
         super(props);
 
+       
+
         this.state = {
+
+
             chartOptions: {
-                title: {
-                },
+                title: "",
 
                 chart: {
                     type: 'spline',
@@ -49,15 +53,30 @@ export default class GraphComponent extends Component {
                             { separator: true },
                             {
                                 text: 'Скачать JSON',
-                                style:{
-                                    fontSize:'20px'
+                                style: {
+                                    fontSize: '20px'
                                 },
                                 onclick: function () {
-                                    this.exportChart({
-                                        type: 'image/png'
-                                    });
-                                },
-                            }],
+
+                                    axios.get('http://localhost:5000/graph', {
+                                        headers: {
+                                            'Access-Control-Allow-Origin': 'http://localhost:3000/graph'
+                                        }
+                                    })
+                                        .then(res => {
+
+                                            // 2. Create blob link to download
+                                            const url = window.URL.createObjectURL(new Blob([JSON.stringify(res)]));
+                                            const link = document.createElement('a');
+                                            link.href = url;
+                                            link.setAttribute('download', `${res.data.title}.json`);  // 3. Append to html page
+                                            document.body.appendChild(link);  // 4. Force download
+                                            link.click();  // 5. Clean up and remove the link
+                                            link.parentNode.removeChild(link);
+                                        })
+                                }
+                            }
+                            ],
 
                             text: 'Скачать',
 
@@ -97,6 +116,7 @@ export default class GraphComponent extends Component {
 
     }
 
+
     setHoverData = (e) => {
 
         this.setState({ hoverData: e.target.category })
@@ -111,6 +131,7 @@ export default class GraphComponent extends Component {
             }
         })
             .then(res => {
+
 
                 this.setState({
 
@@ -140,7 +161,20 @@ export default class GraphComponent extends Component {
 
     }
 
+    showVariant = () => {
+        this.setState({
+            chartOptions: {
+                title: {
+                    text: ""
+                },
+            }
+        })
+    }
 
+    handleSelectChange = (e) => {
+        
+
+    }
 
     render() {
         const { chartOptions } = this.state;
@@ -150,12 +184,28 @@ export default class GraphComponent extends Component {
             <div>
 
                 <h1 ></h1>
-                <h4>Текст</h4>
+                <h4>Графики какой то странной активности солнца или чего то ещё</h4>
 
-                <div class="btn-group">
-                    <button type="button" class="btn btn-success" onClick={this.updateSeries.bind(this)}>Построить график</button>
-
+                <div id="chooseDataForm">
+                    <label for="exampleFormControlSelect1">Выберите данные</label>
+                    <div class="form-row">
+                        <div class="col-5">
+                            <div class="form-group">
+                                <select class="form-control" id="exampleFormControlSelect1" onChange={this.handleSelectChange.bind(this)} >
+                                    <option value="1">Monthly mean total sunspot number</option>
+                                    <option value="2">13-month smoothed monthly total sunspot number</option>
+                                    <option value="3">Standard method prediction (Kalman)</option>
+                                    <option value="4">Combined method prediction (Kalman)</option>
+                                    <option value="5">McNish & Lincoln method prediction (Kalman)</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <button id="startButton" type="button" class="btn btn-success" onClick={this.showVariant.bind(this)}>Построить график</button>
+                        </div>
+                    </div>
                 </div>
+
                 <h4 id="lol" align="center">Текст</h4>
                 <HighchartsReact
                     highcharts={Highcharts}
